2e9dbd5d137cbdba5458e452743cd4b08532a1ee
nalinahuja22 <nalinahuja22@gmail.com>
2020-05-03 14:05:11 -0400

    Project Backup

--- test.c
+++ test.c
@@ -6,12 +6,7 @@ int main() {
   string * b = cstring("asdf");
   string * c = substrn(b, 1, 2);
 
-  string * cpy = copy(b);
-  append(cpy, "asdfasdfadsf");
-  append(a, "henlo");
-
   printf("%s\n", str(a));
   printf("%s\n", str(b));
   printf("%s\n", str(c));
-  printf("%s\n", str(cpy));
 }
