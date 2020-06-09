fe5064f7473ca7c0dc862dc4c31e1edb89251ac7
Nalin Ahuja <nalinahuja22@gmail.com>
2020-06-09 11:25:36 -0400

    Project Backup

--- test.c
+++ test.c
@@ -3,17 +3,8 @@
 #include "cstring.h"
 
 int main() {
-  for (int i = 0; i < 10; i++) {
-    string * new = cstring("hello");
-
-    append(new, "bruh");
-    prepend(new, "asdfasdf");
-    append(new, "bruh");
-
-    printf("%s\n", str(new));
-
-    if (i > 5) {
-      delete(new);
-    }
-  }
+  string * new = cstring("hello");
+  append(new, "bruh");
+  prepend(new, "asdfasdf");
+  append(new, "bruh");
 }
