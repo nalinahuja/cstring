835a7d65b2befbf4e50ff16b17615e4b319ee8e0
Nalin Ahuja <nalinahuja22@gmail.com>
2020-06-09 09:40:47 -0400

    Project Backup

--- cstring.h
+++ cstring.h
@@ -75,7 +75,7 @@ void _remove_struct(string * s) {
   assert((s) && (s->str));
 
   // Find String Structure
-  for (int i = 0; i < max_allocs; ++i) {
+  for (int i = 0; i < num_allocs; ++i) {
     if (allocs[i] == s) {
       allocs[i] = NULL;
       break;
